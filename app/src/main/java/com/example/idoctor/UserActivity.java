96efca83ad9cbc07350d81346e1902562575eca5
dungtntse05695 <dungtntse05695@fpt.edu.vn>
2019-11-07 17:49:04 +0700

    add data user to firebase

--- app/src/main/java/com/example/idoctor/UserActivity.java
+++ app/src/main/java/com/example/idoctor/UserActivity.java
@@ -7,13 +7,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.example.idoctor.model.User;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +26,9 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserActivity extends AppCompatActivity
         implements GoogleApiClient.OnConnectionFailedListener {
@@ -36,6 +44,38 @@ public class UserActivity extends AppCompatActivity
     private FirebaseAuth mFirebaseAuth;
     private FirebaseUser mFirebaseUser;
     private DatabaseReference mDatabaseReference;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference("User");
+        mDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Log.v(TAG,String.valueOf(dataSnapshot.getChildrenCount()));
+                boolean isExist = false;
+                for(DataSnapshot d:dataSnapshot.getChildren()) {
+                    User u = d.getValue(User.class);
+                    if(u.getUid().equals(mFirebaseUser.getUid())) {
+                        isExist = true;
+                    }
+                }
+                if(!isExist) {
+                    User newUser = new User();
+                    newUser.setUid(mFirebaseUser.getUid());
+                    newUser.setPhotoURL(mFirebaseUser.getPhotoUrl().toString());
+                    newUser.setRole(1);
+                    newUser.setName(mFirebaseUser.getDisplayName());
+                    newUser.setEmail(mFirebaseUser.getEmail());
+                    mDatabaseReference.push().setValue(newUser);
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.v(TAG,databaseError.getMessage());
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,7 +95,11 @@ public class UserActivity extends AppCompatActivity
             return;
         } else {
             // get user data from mUID
-            mUID = mFirebaseUser.getUid();
+
+
+
+
+
         }
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
@@ -75,7 +119,6 @@ public class UserActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu,menu);
         return super.onCreateOptionsMenu(menu);
-
     }
 
     @Override
@@ -90,7 +133,6 @@ public class UserActivity extends AppCompatActivity
             default:
                 return super.onOptionsItemSelected(item);
         }
-
     }
 
     @Override

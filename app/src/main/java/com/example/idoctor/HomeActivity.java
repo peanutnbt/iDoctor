6d1118a53b14f2e20c85e05c61b6eadf71ba8cee
= <=>
2019-11-11 13:49:22 +0700

    add home and search pages

---
+++ app/src/main/java/com/example/idoctor/HomeActivity.java
@@ -0,0 +1,106 @@
+package com.example.idoctor;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.GridView;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.idoctor.model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class HomeActivity extends AppCompatActivity {
+
+
+    private GridView gridViewDoctor;
+    private TextView mShowMore;
+    public ArrayList<User> users;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        readData();
+
+        mShowMore = findViewById(R.id.showMoreMess);
+        mShowMore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void readData(){
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        DatabaseReference myRef = database.getReference("User");
+
+        Query lastQuery = myRef.orderByChild("role").equalTo(1).limitToLast(4);
+
+
+        users = new ArrayList<>();
+        System.out.println("DMMMMMMMMMMMMMMMMMMMMM");
+        lastQuery.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                System.out.println("CCCCCCCCCCCCCCCCCCCCCCCC");
+                for(DataSnapshot keyNode : dataSnapshot.getChildren()){
+                    System.out.println("OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"+keyNode);
+                    User user = keyNode.getValue(User.class);
+                    users.add(user);
+                    System.out.println("AAAAAAAAAAA: "+user.getName());
+                }
+
+                createGrid();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                System.out.println("UUUUUUUUUUUUUUUUUUUUUU"+databaseError.toString());
+            }
+        });
+
+    }
+
+    public void createGrid(){
+        gridViewDoctor = (GridView) findViewById(R.id.gridView);
+        GridDoctorAdapter gridDoctorAdapter = new GridDoctorAdapter(this,users);
+        gridViewDoctor.setAdapter(gridDoctorAdapter);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.search_menu_item:
+                Intent intent = new Intent(this, SearchActivity.class);
+                this.startActivity(intent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/idoctor/SearchActivity.java
@@ -0,0 +1,167 @@
+package com.example.idoctor;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.content.Intent;
+import android.app.IntentService;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.example.idoctor.model.User;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.firebase.ui.database.SnapshotParser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+public class SearchActivity extends AppCompatActivity {
+
+    private EditText mSearchField;
+    private ImageView mSearchBtn;
+
+    private RecyclerView mResultList;
+
+    private DatabaseReference mUserDatabase;
+
+    FirebaseRecyclerAdapter<User,UserViewHolder> firebaseRecyclerAdapter;
+
+    boolean searchAll = false;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        mUserDatabase = FirebaseDatabase.getInstance().getReference("User");
+        mResultList = findViewById(R.id.result_list);
+        mSearchBtn = findViewById(R.id.imageView);
+        mSearchField = findViewById(R.id.search_field);
+
+        mResultList.setHasFixedSize(false);
+        mResultList.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+        mSearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), "Search", Toast.LENGTH_SHORT).show();
+                String searchText = mSearchField.getText().toString();
+                firebaseUserSearch(searchText);
+            }
+        });
+
+        TextView search_all_text = findViewById(R.id.findAll_text);
+        search_all_text.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchAll = true;
+                firebaseUserSearch("");
+            }
+        });
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseUserSearch("");
+    }
+    private void firebaseUserSearch(String searchText) {
+        Query firebaseQuery;
+        if(searchAll){
+            searchAll = false;
+            firebaseQuery = mUserDatabase.orderByChild("role").equalTo(1);
+        }
+        else{
+            firebaseQuery = mUserDatabase.orderByChild("name_role").equalTo(searchText + "_1");
+        }
+        System.out.println("here1");
+
+//        Query firebaseQuery= mUserDatabase.orderByChild("role").equalTo(1);
+        FirebaseRecyclerOptions<User> options =
+                new FirebaseRecyclerOptions.Builder<User>()
+                        .setQuery(firebaseQuery, User.class)
+                        .setLifecycleOwner(this)
+                        .build();
+
+
+
+        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<User, UserViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull UserViewHolder holder, int position, @NonNull User model) {
+                System.out.println("here2");
+                holder.setDetails(getApplicationContext(),model);
+            }
+
+            @NonNull
+            @Override
+            public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                System.out.println("here 4");
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.list_search_layout, parent, false);
+
+                return new UserViewHolder(view);
+            }
+        };
+        System.out.println("here3");
+        mResultList.setAdapter(firebaseRecyclerAdapter);
+        System.out.println(mResultList.getAdapter());
+        System.out.println("dm3");
+    }
+
+    public static class UserViewHolder extends RecyclerView.ViewHolder{
+
+        View mView;
+
+        public UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mView=itemView;
+            System.out.println("DM1");
+        }
+        public void setDetails(final Context ctx, final User user){
+            System.out.println("DM2");
+            TextView user_name= mView.findViewById(R.id.name_text);
+            TextView user_status= mView.findViewById(R.id.status_text);
+            ImageView user_image= mView.findViewById(R.id.profile_image);
+
+            user_name.setText(user.getName());
+            user_status.setText(user.getName());
+
+            Glide
+                    .with(ctx)
+                    .load(user.getPhotoURL())
+                    .apply(RequestOptions.circleCropTransform())
+                    .into(user_image);
+
+
+            user_image.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    System.out.println("ALOHA: " + user.getName());
+
+                    Intent ii=new Intent(ctx, TestActivity.class);
+                    ii.putExtra("user", user);
+                    ctx.startActivity(ii);
+                }
+            });
+        }
+    }
+
+}
